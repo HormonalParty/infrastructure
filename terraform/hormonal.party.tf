d1b187f2f095752311e0ee775728b17b813c4cde
Maxine E. Aubrey <maxeaubrey@gmail.com>
2020-02-22 11:06:14 +0100

    dns: add berlin.infra.hormonal.party

--- terraform/hormonal.party.tf
+++ terraform/hormonal.party.tf
@@ -5,3 +5,19 @@ resource "dnsimple_record" "home_hormonal_party" {
   type = "a"
   ttl = 3600
 }
+
+resource "dnsimple_record" "berlin_infra_hormonal_party_a" {
+  domain = "hormonal.party"
+  name = "berlin.infra"
+  value = "178.63.8.90"
+  type = "a"
+  ttl = 3600
+}
+
+resource "dnsimple_record" "berlin_infra_hormonal_party_aaaa" {
+  domain = "hormonal.party"
+  name = "berlin.infra"
+  value = "2a01:4f8:110:437b::1"
+  type = "aaaa"
+  ttl = 3600
+}
