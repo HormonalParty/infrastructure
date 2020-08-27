84973bb3059e98303b3ee5f73f3985960ea63e15
Danielle Lancashire <dani@builds.terrible.systems>
2020-08-27 15:55:06 +0200

    dns: Add grafana.svc.hormonal.party

--- terraform/hormonal.party.tf
+++ terraform/hormonal.party.tf
@@ -2,7 +2,7 @@ resource "dnsimple_record" "home_hormonal_party" {
   domain = "hormonal.party"
   name   = "home"
   value  = "90.187.234.21"
-  type   = "a"
+  type   = "A"
   ttl    = 3600
 }
 
@@ -10,7 +10,7 @@ resource "dnsimple_record" "berlin_infra_hormonal_party_a" {
   domain = "hormonal.party"
   name   = "berlin.infra"
   value  = "178.63.8.90"
-  type   = "a"
+  type   = "A"
   ttl    = 3600
 }
 
@@ -18,6 +18,22 @@ resource "dnsimple_record" "berlin_infra_hormonal_party_aaaa" {
   domain = "hormonal.party"
   name   = "berlin.infra"
   value  = "2a01:4f8:110:437b::1"
-  type   = "aaaa"
+  type   = "AAAA"
+  ttl    = 3600
+}
+
+resource "dnsimple_record" "grafana_svc_hormonal_party_a" {
+  domain = "hormonal.party"
+  name   = "grafana.svc"
+  value  = "178.63.8.90"
+  type   = "A"
+  ttl    = 3600
+}
+
+resource "dnsimple_record" "grafana_svc_hormonal_party_aaaa" {
+  domain = "hormonal.party"
+  name   = "grafana.svc"
+  value  = "2a01:4f8:110:437b::1"
+  type   = "AAAA"
   ttl    = 3600
 }
