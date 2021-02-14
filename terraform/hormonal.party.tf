e55a2de64c752d3414a45ccc69ee72afd310773f
Danielle Lancashire <dani@builds.terrible.systems>
2021-02-14 21:52:40 +0100

    grafana.hormonal.party: move to home cluster

--- terraform/hormonal.party.tf
+++ terraform/hormonal.party.tf
@@ -22,19 +22,11 @@ resource "dnsimple_record" "berlin_infra_hormonal_party_aaaa" {
   ttl    = 3600
 }
 
-resource "dnsimple_record" "grafana_svc_hormonal_party_a" {
+resource "dnsimple_record" "grafana_hormonal_party_a" {
   domain = "hormonal.party"
-  name   = "grafana.svc"
-  value  = "178.63.8.90"
+  name   = "grafana"
+  value  = "90.187.234.21"
   type   = "A"
-  ttl    = 3600
-}
-
-resource "dnsimple_record" "grafana_svc_hormonal_party_aaaa" {
-  domain = "hormonal.party"
-  name   = "grafana.svc"
-  value  = "2a01:4f8:110:437b::1"
-  type   = "AAAA"
   ttl    = 3600
 }
 
