bb58af6df7c30bb6fdf66e59246a16261d47dbf9
Maxine Aubrey <maxeaubrey@gmail.com>
2021-08-26 21:23:03 +0200

    mail.hormonal.party: init
    including related mx/spf/dkim/dmarc records

--- terraform/hormonal.party.tf
+++ terraform/hormonal.party.tf
@@ -53,3 +53,43 @@ resource "dnsimple_record" "photos_hormonal_party_a" {
   type   = "A"
   ttl    = 3600
 }
+
+resource "dnsimple_record" "mail_hormonal_party_a" {
+  domain = "hormonal.party"
+  name   = "mail"
+  value  = "90.187.234.21"
+  type   = "A"
+  ttl    = 3600
+}
+
+resource "dnsimple_record" "mail_hormonal_party_mx" {
+  domain = "hormonal.party"
+  name   = ""
+  value  = "10 mail.hormonal.party"
+  type   = "MX"
+  ttl    = 3600
+}
+
+resource "dnsimple_record" "mail_hormonal_party_spf" {
+  domain = "hormonal.party"
+  name   = ""
+  value  = "v=spf1 a:mail.hormonal.party -all"
+  type   = "TXT"
+  ttl    = 10800
+}
+
+resource "dnsimple_record" "mail_hormonal_party_dkim" {
+  domain = "hormonal.party"
+  name   = "mail._domainkey"
+  value  = "v=DKIM1; k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDnfACG7rRBXXa0bqiD8gJ3aNUEfK3nrMWZX3lPCT0R9liGAAuIfuYo08Dlcn1wcvjly2V95rwu+Af8/QW5M/KBVV7DJOYqOHBvxpEObkdwbASlAyo10HbUzuhgjPDtLx8/zALUoB4sgnI3oS4yz6ASXbizJ7Ochqa1DDN271bECQIDAQAB"
+  type   = "TXT"
+  ttl    = 10800
+}
+
+resource "dnsimple_record" "mail_hormonal_party_dmarc" {
+  domain = "hormonal.party"
+  name   = "_dmarc"
+  value  = "v=DMARC1; p=none"
+  type   = "TXT"
+  ttl    = 10800
+}
