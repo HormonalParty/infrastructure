72b5b41e6880f9c892a7d6486f7759328b31c260
Maxine Aubrey <maxeaubrey@gmail.com>
2022-03-03 19:22:01 +0100

    tf: clean zone up

--- terraform/hormonal.party.tf
+++ terraform/hormonal.party.tf
@@ -42,34 +42,10 @@ resource "dnsimple_zone_record" "lisbon_infra_hormonal_party_aaaa" {
   ttl       = 3600
 }
 
-resource "dnsimple_zone_record" "grafana_hormonal_party_a" {
-  zone_name = "hormonal.party"
-  name      = "grafana"
-  value     = local.home_v4
-  type      = "A"
-  ttl       = 3600
-}
-
 resource "dnsimple_zone_record" "homeassistant_hormonal_party_a" {
   zone_name = "hormonal.party"
   name      = "homeassistant"
   value     = "192.168.2.132"
-  type      = "A"
-  ttl       = 3600
-}
-
-resource "dnsimple_zone_record" "goproxy_hormonal_party_a" {
-  zone_name = "hormonal.party"
-  name      = "goproxy"
-  value     = local.home_v4
-  type      = "A"
-  ttl       = 3600
-}
-
-resource "dnsimple_zone_record" "photos_hormonal_party_a" {
-  zone_name = "hormonal.party"
-  name      = "photos"
-  value     = local.home_v4
   type      = "A"
   ttl       = 3600
 }
