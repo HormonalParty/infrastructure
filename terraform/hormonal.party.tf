cc2745c82b2275306aa7e971cee9dac51e2f6f62
Danielle Lancashire <dani@builds.terrible.systems>
2022-10-07 01:51:13 +0200

    hormonal.party: add goproxy.tailnet

--- terraform/hormonal.party.tf
+++ terraform/hormonal.party.tf
@@ -1,5 +1,6 @@
 locals {
-  home_v4 = "79.252.116.229"
+  home_v4           = "79.252.116.229"
+  sintra_tailnet_v4 = "100.94.89.26"
 }
 
 resource "dnsimple_zone_record" "home_hormonal_party" {
@@ -15,6 +16,14 @@ resource "dnsimple_zone_record" "plex_hormonal_party" {
   name      = "plex"
   value     = dnsimple_zone_record.home_hormonal_party.qualified_name
   type      = "CNAME"
+  ttl       = 3600
+}
+
+resource "dnsimple_zone_record" "goproxy_tailnet_hormonal_party" {
+  zone_name = "hormonal.party"
+  name      = "goproxy.tailnet"
+  value     = local.sintra_tailnet_v4
+  type      = "A"
   ttl       = 3600
 }
 
