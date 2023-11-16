69998f413acb487d73b98d5676c1d56ab42727a7
Danielle Lancashire <dani@builds.terrible.systems>
2023-11-17 00:59:04 +0100

    add git.hormonal.party

--- terraform/hormonal.party.tf
+++ terraform/hormonal.party.tf
@@ -43,6 +43,14 @@ resource "dnsimple_zone_record" "lisbon_infra_hormonal_party_aaaa" {
   ttl       = 3600
 }
 
+resource "dnsimple_zone_record" "git_hormonal_party_cname" {
+  zone_name = "hormonal.party"
+  name      = "git"
+  value     = "lisbon.infra.hormonal.party"
+  type      = "CNAME"
+  ttl       = 3600
+}
+
 resource "dnsimple_zone_record" "homeassistant_hormonal_party_a" {
   zone_name = "hormonal.party"
   name      = "homeassistant"
